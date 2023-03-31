c46c5cf32e9a58dba6b5b20189e9b24490d162bc
Jakub Skoczylas <skoczylasjakub1984@gmail.com>
2023-03-31 13:23:44 +0200

    Zadanie domowe z dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+string sex = "female";
+var age = 33;
+
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (sex == "female" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
